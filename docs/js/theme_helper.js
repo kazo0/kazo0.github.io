dfd5bc62dd855c4dbd1825df5a5e49c1c327d4dc
Steve Bilogan <steve.bilogan@gmail.com>
2023-11-13 21:44:19 -0500

    theme

--- docs/js/theme_helper.js
+++ docs/js/theme_helper.js
@@ -1,12 +1,36 @@
+function ready(fn) {
+  if (document.readyState !== 'loading') {
+    fn();
+    return;
+  }
+  document.addEventListener('DOMContentLoaded', fn);
+}
+
+ready(function(){
+  let enabled = localStorage.getItem('dark-mode')
+  let audio = document.createElement('audio');
+
+  if (enabled === null) {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        enable();
+    }
+  } else if (enabled === 'true') {
+    enable()
+  }
+});
+
+
 function enable()  {
     DarkReader.setFetchMethod(window.fetch)
     DarkReader.enable();
     localStorage.setItem('dark-mode', 'true');
+    document.getElementById('icon-dark').className = "fa fa-sun"
   }
   
   function disable() {
     DarkReader.disable();
     localStorage.setItem('dark-mode', 'false');
+    document.getElementById('icon-dark').className = "fa fa-moon"
   }
   
   function darkmode() {
