d2fb14670b1df27664de5cb25361765a7a6645f2
Steve Bilogan <steve.bilogan@gmail.com>
2023-11-28 12:38:22 -0500

    fix theme toggling

--- docs/js/theme_helper.js
+++ docs/js/theme_helper.js
@@ -10,19 +10,55 @@ ready(function(){
   document.getElementById('theme-toggle').onclick = function() {
     darkmode()
   }
+  let theme = localStorage.getItem('theme')
+
+  if (theme === null) {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        enable();
+    }
+  } else if (theme === "dark") {
+    enable()
+  }
+
+  if (localStorage.getItem('theme') === "light") {
+    disable();
+  } else {
+      enable();
+  }
 });
   
-  function darkmode() {
+  function enable()  {
+    localStorage.setItem('theme', 'dark');
+    document.getElementById('icon-dark').className = "fa fa-sun"
     node1 = document.getElementById('theme_source');
     node2 = document.getElementById('theme_source_dark');
-    if (node1.getAttribute('rel') == 'stylesheet') {
-      node1.setAttribute('rel', 'stylesheet alternate');
-      node2.setAttribute('rel', 'stylesheet');
-      sessionStorage.setItem('theme', 'dark');
+    node1.setAttribute('rel', 'stylesheet alternate'); 
+    node2.setAttribute('rel', 'stylesheet');
+  }
+
+  function disable() {
+    node1 = document.getElementById('theme_source');
+    node2 = document.getElementById('theme_source_dark');
+    node2.setAttribute('rel', 'stylesheet alternate');
+    node1.setAttribute('rel', 'stylesheet');
+    localStorage.setItem('theme', 'light');
+    document.getElementById('icon-dark').className = "fa fa-moon"
+  }
+
+  function darkmode() {
+    let theme = localStorage.getItem('theme')
+
+    if (theme === null) {
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+          enable();
+      }
+    } else if (theme === "dark") {
+      enable()
+    }
+
+    if (localStorage.getItem('theme') === "light") {
+        enable();
     } else {
-      node2.setAttribute('rel', 'stylesheet alternate');
-      node1.setAttribute('rel', 'stylesheet');
-      sessionStorage.setItem('theme', 'light');
+        disable();
     }
-    return false;
-    }
+  }
